e10ecd6b2ada2de9fbe98e05918e6d658a6cf0a0
hyparh <hyparh@gmail.com>
2023-02-17 14:57:24 +0200

    Created mock heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Barbarian' },
+  { id: 13, name: 'Amazon' },
+  { id: 14, name: 'Assassin' },
+  { id: 15, name: 'Druid' },
+  { id: 16, name: 'Necromancer' },
+  { id: 17, name: 'Sorceress' },
+  { id: 18, name: 'Paladin' },
+  { id: 19, name: 'Rogue' },
+  { id: 20, name: 'Witch Doctor' }
+];
